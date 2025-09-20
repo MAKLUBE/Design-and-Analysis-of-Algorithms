55d7550b86aa608a8ee8f102e1e0e70e795a3214
MAKLUBE <semenovrasul7@gmail.com>
2025-09-20 17:02:37 +0500

    Update and rename Test.java to NewTest.java

--- src/test/java/org/example/NewTest.java
+++ src/test/java/org/example/NewTest.java
@@ -3,6 +3,8 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class Test {
-    @Test void adds() { assertEquals(4, 2 + 2); }
+class NewTest {
+    @Test void adds() { 
+        assertEquals(4, 2 + 2);
+    }
 }
